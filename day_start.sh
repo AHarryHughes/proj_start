896fcb13ccad0c14c86bcec718101f6b9ffce85b
AHarryHughes <Ahhughes8@gmail.com>
2017-06-01 20:02:32 -0400

    Version3 - problems with line 36... almost done... hopefully

--- day_start.sh
+++ day_start.sh
@@ -10,31 +10,47 @@ echo $_MR_FILE
 cd $_CUR_DIR
 
 VAR_loop=0
+myarray=
 for word in $_MR_FILE
 do
-	myarray[VAR_loop]=“$word"
-	VAR_loop=$VAR_loop+1
+	myarray[$VAR_loop]=$word
+	echo ${myarray[$VAR_loop]}
+	let "VAR_loop++"
+	echo $VAR_loop
 done
-
+echo $VAR_loop
 VAR_count=0
-VAR_spaceSlash="\\ "
-while [${myarray[@] -lt VAR_count]
+VAR_spaceSlash='\\ '
+echo $VAR_spaceSlash
+_TMP=
+while [ $VAR_loop -gt $VAR_count ]
 do
-	if [VAR_count -eq 0]
+	echo "run while"
+	echo $VAR_count
+	if [ $VAR_count -eq 0 ]
 	then
-		_MR_FILE=${myarray[0]}$VAR_spaceSlash
+		_TMP=${myarray[0]}$VAR_spaceSlash
+		echo $_TMP
+		echo "first if"
 	fi
-	if [VAR_count -eq ${myarray[@]}-1]
+	if [ $VAR_count -eq $expr $VAR_loop - 1 ]
 	then
-		_MR_FILE=$_MR_FILE${myarray[$VAR_count]}
+		_TMP=$_TMP${myarray[$VAR_count]}
+		echo $_TMP
+		echo "second if"
+	else
+		_TMP=$_TMP${myarray[$VAR_count]}$VAR_spaceSlash
 	fi
-	MR_FILE=$MR_FILE${myarray[$VAR_count]}$VAR_spaceSlash
+	echo $_TMP
+	let "VAR_count++"
 done
+echo $_TMP
+echo $_MR_FILE
 
-mv ~/Downloads/$_MR_FILE .
-unzip $_MR_FILE
-rm $_MR_FILE
-open $find *.html
-atom .
+#mv ~/Downloads/$_MR_FILE .
+#unzip $_MR_FILE
+#rm $_MR_FILE
+#open $find *.html
+#atom .
 }
 day_start
